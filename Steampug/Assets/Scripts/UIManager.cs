56fb2d98ec865998c14a406ad26c8ba4b7be75b8
sejour-a <sejour_a@jormungandr.eu>
2016-02-23 04:17:07 +0100

    Added the main menu.

---
+++ Steampug/Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿///////////////////////////
+// Default Unity Include //
+using UnityEngine;
+using System.Collections;
+
+///////////////////////////////
+// Used by UIManager.Play () //
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void Quit ()
+	{
+		Application.Quit();
+	}
+
+	public void Play ()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+}
